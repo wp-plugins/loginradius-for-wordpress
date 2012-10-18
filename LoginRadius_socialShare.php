8bc05756b825c5361d3bf34088d0401254719143
loginradius <loginradius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-18 05:37:14 +0000

    WordPress theme related issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/loginradius-for-wordpress/tags/3.1@613937 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- LoginRadius_socialShare.php
+++ LoginRadius_socialShare.php
@@ -1,114 +1,82 @@
-<?php 
-
-
-
-  	add_action('wp_enqueue_scripts', 'loginradius_share_output');
-
+<?php  
+  	add_action('wp_enqueue_scripts', 'loginradius_share_output'); 
 
 
 		if( $LoginRadius_settings['LoginRadius_shareEnable'] == "1" ){
 
-	
+ 			if(isset( $LoginRadius_settings['LoginRadius_sharehome'] ) || isset( $LoginRadius_settings['LoginRadius_sharepost'] ) || isset( $LoginRadius_settings['LoginRadius_sharepage'] ) || isset( $LoginRadius_settings['LoginRadius_shareexcerpt'] ) || isset( $LoginRadius_settings['LoginRadius_sharearchive'] ) || isset( $LoginRadius_settings['LoginRadius_sharefeed'] ) )
 
-			if($LoginRadius_settings['LoginRadius_sharehome'] || $LoginRadius_settings['LoginRadius_sharepost'] || $LoginRadius_settings['LoginRadius_sharepage'] || $LoginRadius_settings['LoginRadius_shareexcerpt'] || $LoginRadius_settings['LoginRadius_sharearchive'] || $LoginRadius_settings['LoginRadius_sharefeed'])
+ 			{
 
-		
+ 				function loginRadiusShareContent($content) 
 
-			{
+ 				{
 
-	
+ 				global $LoginRadius_settings;
 
-				function loginRadiusShareContent($content) 
+ 				
 
-		
 
-				{
 
 	
 
-				global $LoginRadius_settings;
-
-	
-
-				
-
-	
 				if (strpos($LoginRadius_settings['LoginRadius_shareCode'], 'horizontal') !== false) {
-					$append = "<div style='margin:0'><b>".ucfirst($LoginRadius_settings['LoginRadius_share_title'])."</b></div><div class='lrsharecontainer'></div>";
-				}else{
-					$append = "<div class='lrsharecontainer'></div>";
-				}
-	
-	
 
-				if( ( $LoginRadius_settings['LoginRadius_sharehome'] && is_front_page() ) || ( $LoginRadius_settings['LoginRadius_sharepost'] && is_single() ) || ( $LoginRadius_settings['LoginRadius_sharepage'] && is_page() ) || ( $LoginRadius_settings['LoginRadius_shareexcerpt'] && has_excerpt() ) || ( $LoginRadius_settings['LoginRadius_sharearchive'] && is_archive() ) || ( $LoginRadius_settings['LoginRadius_sharefeed'] && is_feed() ) )
+					$append = "<div style='margin:0'><b>".ucfirst($LoginRadius_settings['LoginRadius_share_title'])."</b></div><div class='lrsharecontainer'></div>";
+
+				}else{
+
+					$append = "<div class='lrsharecontainer'></div>";
+
+				}
+
+	 				if( ( isset( $LoginRadius_settings['LoginRadius_sharehome'] ) && is_front_page() ) || ( isset( $LoginRadius_settings['LoginRadius_sharepost'] ) && is_single() ) || ( isset( $LoginRadius_settings['LoginRadius_sharepage'] ) && is_page() ) || ( isset( $LoginRadius_settings['LoginRadius_shareexcerpt'] ) && has_excerpt() ) || ( isset( $LoginRadius_settings['LoginRadius_sharearchive'] ) && is_archive() ) || ( isset( $LoginRadius_settings['LoginRadius_sharefeed'] ) && is_feed() ) )
+
 				{	
 
-					if($LoginRadius_settings['LoginRadius_sharetop'] && $LoginRadius_settings['LoginRadius_sharebottom'])
+
+
+					if( isset( $LoginRadius_settings['LoginRadius_sharetop'] ) && isset( $LoginRadius_settings['LoginRadius_sharebottom'] ) )
+
 					{
+
 						$content = $append.'<br/>'.$content.'<br/>'.$append;
+
 					}
+
 					else{
 
-	
 
-						if($LoginRadius_settings['LoginRadius_sharetop'])
+						if( isset( $LoginRadius_settings['LoginRadius_sharetop'] ) )
 
-	
 
 						{
 
-	
+ 							$content = $append.$content;
 
-							$content = $append.$content;
+ 						}
 
-	
+ 						elseif( isset( $LoginRadius_settings['LoginRadius_sharebottom'] ) )
 
-						}
+ 						{
 
-	
+ 							$content = $content.$append;
 
-						elseif($LoginRadius_settings['LoginRadius_sharebottom'])
+ 						}
 
-	
+ 					}
 
-						{
+ 				}
 
-	
+ 			  return $content;
 
-							$content = $content.$append;
+ 			}
 
-	
+ 			add_filter('the_content', 'loginRadiusShareContent');
 
-						}
-
-	
-
-					}
-
-	
-
-				}
-
-	
-
-			  return $content;
-
-	
-
-			}
-
-	
-
-			add_filter('the_content', 'loginRadiusShareContent');
-
-	
-
-		}
+ 		} 
 
 
-
-	}
-
+	} 
 
 
