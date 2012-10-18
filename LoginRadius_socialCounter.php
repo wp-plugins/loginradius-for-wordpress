50ac0df0d746656b66e2a932619f0fa880594931
loginradius <loginradius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-18 05:37:14 +0000

    WordPress theme related issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/loginradius-for-wordpress/trunk@613937 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- LoginRadius_socialCounter.php
+++ LoginRadius_socialCounter.php
@@ -1,142 +1,80 @@
-<?php 
+<?php  
 
-
-
-  	add_action('wp_enqueue_scripts', 'loginradius_counter_output');
-
-
+  	add_action('wp_enqueue_scripts', 'loginradius_counter_output'); 
 
 		if( $LoginRadius_settings['LoginRadius_counterEnable'] == "1" ){
 
-	
+ 			if( isset( $LoginRadius_settings['LoginRadius_counterhome'] ) || isset( $LoginRadius_settings['LoginRadius_counterpost'] ) || isset( $LoginRadius_settings['LoginRadius_counterpage'] ) || isset( $LoginRadius_settings['LoginRadius_counterexcerpt'] ) || isset( $LoginRadius_settings['LoginRadius_counterarchive'] ) || isset( $LoginRadius_settings['LoginRadius_counterfeed'] ) )
 
-			if($LoginRadius_settings['LoginRadius_counterhome'] || $LoginRadius_settings['LoginRadius_counterpost'] || $LoginRadius_settings['LoginRadius_counterpage'] || $LoginRadius_settings['LoginRadius_counterexcerpt'] || $LoginRadius_settings['LoginRadius_counterarchive'] || $LoginRadius_settings['LoginRadius_counterfeed'])
+ 			{
 
-		
+ 				function loginRadiusCounterContent($content) 
 
-			{
+ 				{
 
-	
+ 				global $LoginRadius_settings;
 
-				function loginRadiusCounterContent($content) 
+ 				
 
-		
 
-				{
-
-	
-
-				global $LoginRadius_settings;
-
-	
-
-				
 
 				if (strpos($LoginRadius_settings['LoginRadius_counterCode'], 'isHorizontal = true') !== false) {
 
+
+
 					$append = "<div style='margin:0'><b>".ucfirst($LoginRadius_settings['LoginRadius_counter_title'])."</b></div><br/><div class='lrcounter_simplebox'></div>";
+
 				}else{
+
 					$append = "<div class='lrcounter_simplebox'></div>";
-				}
-	
 
-					
+				} 					
 
-	
+ 				if( ( isset( $LoginRadius_settings['LoginRadius_counterhome'] ) && is_front_page() ) || ( isset( $LoginRadius_settings['LoginRadius_counterpost'] ) && is_single() ) || ( isset( $LoginRadius_settings['LoginRadius_counterpage'] ) && is_page() ) || ( isset( $LoginRadius_settings['LoginRadius_counterexcerpt'] ) && has_excerpt() ) || ( isset( $LoginRadius_settings['LoginRadius_counterarchive'] ) && is_archive() ) || ( isset( $LoginRadius_settings['LoginRadius_counterfeed'] ) && is_feed() ) )
 
-				if( ( $LoginRadius_settings['LoginRadius_counterhome'] && is_front_page() ) || ( $LoginRadius_settings['LoginRadius_counterpost'] && is_single() ) || ( $LoginRadius_settings['LoginRadius_counterpage'] && is_page() ) || ( $LoginRadius_settings['LoginRadius_counterexcerpt'] && has_excerpt() ) || ( $LoginRadius_settings['LoginRadius_counterarchive'] && is_archive() ) || ( $LoginRadius_settings['LoginRadius_counterfeed'] && is_feed() ) )
+ 				{	
 
-	
+ 				
 
-				{	
+ 					if( isset( $LoginRadius_settings['LoginRadius_countertop'] ) && isset( $LoginRadius_settings['LoginRadius_counterbottom'] ) )
 
-	
+ 					{
 
-				
+ 						$content = $append.'<br/>'.$content.'<br/>'.$append;
 
-	
+ 					}
 
-					if($LoginRadius_settings['LoginRadius_countertop'] && $LoginRadius_settings['LoginRadius_counterbottom'])
+ 					else
 
-	
+ 					{
 
-					{
+ 						if( isset( $LoginRadius_settings['LoginRadius_countertop'] ) )
 
-	
+ 						{
 
-						$content = $append.'<br/>'.$content.'<br/>'.$append;
+ 							$content = $append.$content;
 
-	
+ 						}
 
-					}
+ 						elseif( isset( $LoginRadius_settings['LoginRadius_counterbottom'] ) )
 
-	
+ 						{
 
-					else
+ 							$content = $content.$append;
 
-	
+ 						}
 
-					{
+ 					}
 
-	
+ 				}
 
-						if($LoginRadius_settings['LoginRadius_countertop'])
+ 			  return $content;
 
-	
+ 			}
 
-						{
+ 			add_filter('the_content', 'loginRadiusCounterContent');
 
-	
+ 		} 
 
-							$content = $append.$content;
-
-	
-
-						}
-
-	
-
-						elseif($LoginRadius_settings['LoginRadius_counterbottom'])
-
-	
-
-						{
-
-	
-
-							$content = $content.$append;
-
-	
-
-						}
-
-	
-
-					}
-
-	
-
-				}
-
-	
-
-			  return $content;
-
-	
-
-			}
-
-	
-
-			add_filter('the_content', 'loginRadiusCounterContent');
-
-	
-
-		}
-
-
-
-	}
-
-
+	} 
 
