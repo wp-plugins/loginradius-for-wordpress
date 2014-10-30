e420e604c2ed7bc92c7627e7f1fac617793226c5
loginradius-dev <loginradius-dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 20:42:16 +0000

    Social Login 6.1.2
    git-svn-id: https://plugins.svn.wordpress.org/loginradius-for-wordpress/tags/6.1.2@1016992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loginradius-social-login/common/loginradius-ajax.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * populate email asking popup
+ */
+function login_radius_email_popup() {
+    if ( isset( $_GET['key'] ) && $_GET['key'] != '' ) {
+        global $wpdb;
+        $loginRadiusTempUserId = $wpdb->get_var( $wpdb->prepare( 'SELECT user_id FROM ' . $wpdb->usermeta . ' WHERE meta_key=\'tmpsession\' AND meta_value = %s', $_GET['key'] ) );
+        $provider = get_user_meta( $loginRadiusTempUserId, 'tmpProvider', true );
+        ?>
+        <div class="LoginRadius_overlay" id="fade">
+            <div id="popupouter">
+                <div class="lr-popupheading"> You are trying to connect with <?php echo $provider; ?></div>
+                <div id="popupinner">
+                    <div id="loginRadiusError" style = "display: none;"></div>
+                        <?php
+                        if( $_GET['isError'] == 'yes' ) {
+                            echo '<div id="textmatter" class="lr-noerror" style = "background-color: rgb(255, 235, 232);border: 1px solid rgb(204, 0, 0);">';
+                        } else {
+                            echo '<div id="textmatter" class="lr-noerror">';
+                        }
+                        if ( isset( $_GET['message'] ) && $_GET['message'] != '' ) {
+                            echo str_replace(  '@provider', $provider, $_GET['message'] );
+                        }
+                        ?>
+                    </div>
+                    <div class="emailtext" id="innerp">Enter your email:</div>
+                    <form method="post" action='' onsubmit='return loginRadiusValidateEmail()' style = "height: 50px;">
+                        <div><input type="text" name="email" id="loginRadiusEmail" class="inputtxt" style = "padding-top: 0px;"/></div>
+                        <div class="footerbox">
+                            <input type="submit" id="LoginRadius_popupSubmit" name="LoginRadius_popupSubmit" value="Submit" class="inputbutton">
+                            <input type="hidden" value="<?php echo $_GET['key'] ?>" name = "session"/>
+                        
+                    </form>
+                    <form method = "post">
+                            <input type="submit" name="LoginRadius_popupSubmit" value="Cancel" class="inputbutton" />
+                            <input type="hidden" value="<?php echo $_GET['key'] ?>" name = "session"/>
+                    </form>
+                        </div><!-- END FOOTERBOX -->
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    die;
+}
+
+add_action( 'wp_ajax_nopriv_login_radius_email_popup', 'login_radius_email_popup' );
+
+/**
+ * Function that displaying notification.
+ */
+function login_radius_notification_popup() {
+    ?>
+    <script>
+        jQuery('#TB_title').hide();
+        jQuery().css({'width' : '0'});
+    </script>
+    <div class="LoginRadius_overlay" id="fade">
+        <div id="popupouter">
+            <div id="popupinner">
+                <div id="textmatter">
+                    <?php
+                    if ( isset( $_GET['message'] ) && $_GET['message'] != '' ) {
+                        echo $_GET['message'];
+                    }
+                    ?>
+                </div>
+                <?php
+                if ( isset( $_GET['redirection'] ) && $_GET['redirection'] != '' ) {
+                    ?>
+                    <form method="post" action=''>
+                        <div>
+                            <input type="button" value="OK" class="inputbutton" onclick="location.href = '<?php echo $_GET['redirection'] ?>'">
+                        </div>
+                    </form>
+
+                    <?php
+                } else {
+                    ?>
+                    <form method="post" action="<?php echo site_url() ?>">
+                        <div>
+                            <input type="submit" value="OK" class="inputbutton">
+                        </div>
+                    </form>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    <?php
+    die;
+}
+
+add_action( 'wp_ajax_nopriv_login_radius_notification_popup', 'login_radius_notification_popup' );
+
+// change user status
+function login_radius_change_user_status() {
+    $currentStatus = $_POST['current_status'];
+    $userId = $_POST['user_id'];
+    if ( $currentStatus == '1' ) {
+        update_user_meta( $userId, 'loginradius_status', '0' );
+        die( 'done' );
+    } elseif ( $currentStatus == '0' ) {
+        update_user_meta( $userId, 'loginradius_status', '1' );
+        $user = get_userdata( $userId );
+        $userName = $user->display_name != '' ? $user->display_name : $user->user_nicename;
+        $username = $userName != '' ? ucfirst( $userName ) : ucfirst( $user->user_login );
+        try {
+            Login_Radius_Common::login_radius_send_verification_email( $user->user_email, '', '', 'activation', $username );
+        } catch ( Exception $e ) {
+            die( 'error' );
+        }
+        die( 'done' );
+    }
+}
+
+add_action( 'wp_ajax_login_radius_change_user_status', 'login_radius_change_user_status' );
---
+++ uninstall.php
@@ -0,0 +1,35 @@
+<?php
+
+//if uninstall not called from WordPress, exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+if( !is_multisite()) {
+    $loginRadiusSettings = get_option( 'LoginRadius_settings' );
+    if ( $loginRadiusSettings['delete_options'] == 1 ) {
+        delete_loginradius_options();
+    }
+} else {
+    global $wpdb;
+    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+    $original_blog_id = get_current_blog_id();
+    foreach ( $blog_ids as $blog_id ) {
+        switch_to_blog( $blog_id );
+        $loginRadiusSettings = get_option( 'LoginRadius_settings' );
+        if ( $loginRadiusSettings['delete_options'] == 1 ) {
+            delete_loginradius_options();
+        }
+    }
+    switch_to_blog( $original_blog_id );
+}   
+
+function delete_loginradius_options() {
+    global $wpdb;
+    delete_option( 'LoginRadius_settings' );
+    delete_option( 'LoginRadius_API_settings' );
+    delete_option( 'LoginRadius_share_settings' );
+    delete_option( 'loginradius_db_version' );
+    delete_option( 'loginradius_version' );
+    $wpdb->query( "delete from $wpdb->usermeta where meta_key like 'loginradius%'" );
+}
